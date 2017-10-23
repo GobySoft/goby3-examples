97687998a77a2213b40c6efcb8aca9e183018d85
Toby Schneider <toby@gobysoft.org>
2017-10-23 16:27:45 -0400

    Removed DCCL tags from gps.proto to simplify this example. Added working plugin for goby_moos_gateway

--- src/multi-thread/gps_driver/moos_plugin.cpp
+++ src/multi-thread/gps_driver/moos_plugin.cpp
@@ -7,10 +7,10 @@ using protobuf::GPSPosition;
 using goby::glog;
 using namespace goby::common::logger;
 
-class NavTranslation : public goby::moos::Translator<NavTranslation>
+class NavTranslation : public goby::moos::Translator
 {
 public:
-    using Base = goby::moos::Translator<NavTranslation>;
+    using Base = goby::moos::Translator;
     
     NavTranslation(const GobyMOOSGatewayConfig& cfg) :
         Base(cfg)
@@ -19,8 +19,6 @@ public:
                 [this](const GPSPosition& pos) { this->goby_to_moos(pos); }
                 );                
 
-            Base::moos_comms().Register("TEST_LATITUDE");
-            Base::moos_comms().Register("TEST_LONGITUDE");
             Base::add_moos_trigger("TEST_LATITUDE");
             Base::add_moos_buffer("TEST_LONGITUDE");
             
@@ -29,7 +27,7 @@ public:
 private:
     
     void moos_to_goby(const CMOOSMsg& moos_msg) override
-        {
+        {   
             const auto& key = moos_msg.GetKey();
 
             if(key == "TEST_LATITUDE")
@@ -41,17 +39,21 @@ private:
                     pos.set_latitude(moos_msg.GetDouble());
                     pos.set_longitude(Base::moos_buffer().at("TEST_LONGITUDE").GetDouble());
                     Base::goby_comms().publish<groups::gps_data>(pos);
+                    glog.is(DEBUG1) && glog << "Goby: gps_data: " << pos.ShortDebugString() << std::endl;
                 }
                 catch(std::exception& e)
                 {
-                    glog.is(DEBUG1) && glog << "Failed to create GPSPosition from TEST_LATITUDE and TEST_LONGITUDE. Make sure both are published?" << std::endl;    
+                    glog.is(DEBUG1) && glog << "Failed to create GPSPosition from TEST_LATITUDE and TEST_LONGITUDE. Make sure both are published? " << e.what() << std::endl;
                 }
             }
         }
 
     void goby_to_moos(const GPSPosition& pos)
         {
+            glog.is(DEBUG1) && glog << "MOOS: GPS_LATITUDE: " << pos.latitude() << std::endl;
             Base::moos_comms().Notify("GPS_LATITUDE", pos.latitude());
+            
+            glog.is(DEBUG1) && glog << "MOOS: GPS_LONGITUDE: " << pos.longitude() << std::endl;
             Base::moos_comms().Notify("GPS_LONGITUDE", pos.longitude());
         }
 };
@@ -59,13 +61,9 @@ private:
 extern "C"
 {
     void goby3_moos_gateway_load(goby::MultiThreadApplication<GobyMOOSGatewayConfig>* handler)
-    {
-        handler->launch_thread<NavTranslation>();
-    }
+    { handler->launch_thread<NavTranslation>(); }
     
     void goby3_moos_gateway_unload(goby::MultiThreadApplication<GobyMOOSGatewayConfig>* handler)
-    {
-        handler->join_thread<NavTranslation>();
-    }
+    { handler->join_thread<NavTranslation>(); }
 }
 
