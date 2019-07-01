ceda383d1ddc031264bc9ed90e57fc92a135b8d4
Toby Schneider <toby@gobysoft.org>
2019-07-01 22:24:54 +0200

    Updates to intervehicle

--- src/intervehicle/basic_publisher/publisher.cpp
+++ src/intervehicle/basic_publisher/publisher.cpp
@@ -1,3 +1,4 @@
+#include "goby/middleware/protobuf/intervehicle.pb.h"
 #include "goby/zeromq/single-thread-application.h"
 
 #include "messages/groups.h"
@@ -8,11 +9,27 @@
 // optional "using" declaration (reduces verbiage)
 using Base = goby::zeromq::SingleThreadApplication<BasicPublisherConfig>;
 using dccl::NavigationReport;
+using goby::middleware::intervehicle::protobuf::AckData;
+using goby::middleware::intervehicle::protobuf::ExpireData;
 
 class BasicPublisher : public Base
 {
   public:
-    BasicPublisher() : Base(0.1 * boost::units::si::hertz) {}
+    BasicPublisher() : Base(0.1 * boost::units::si::hertz)
+    {
+        auto on_nav_ack = [](const NavigationReport& nav, const AckData& ack) {
+            std::cout << "Received acknowledgment:\n\t" << ack.ShortDebugString()
+                      << "\nfor message:\n\t" << nav.ShortDebugString() << std::endl;
+        };
+
+        auto on_nav_expired = [](const NavigationReport& nav, const ExpireData& expire) {
+            std::cout << "Received expiration notice:\n\t" << expire.ShortDebugString()
+                      << "\nfor subscription:\n\t" << nav.ShortDebugString() << std::endl;
+        };
+
+        
+        nav_publisher_ = {{}, on_nav_ack, on_nav_expired};
+    }
 
     void loop() override
     {
@@ -21,9 +38,13 @@ class BasicPublisher : public Base
         nav.set_y(195 + std::rand() % 20);
         nav.set_z(-305 + std::rand() % 10);
 
-        std::cout << "Tx: " << nav.DebugString() << std::flush;
-        intervehicle().publish<groups::nav>(nav);
+        std::cout << "\nPublished: " << nav.ShortDebugString() << std::endl;
+
+        intervehicle().publish<groups::nav>(nav, nav_publisher_);
     }
+
+  private:
+    goby::middleware::Publisher<NavigationReport> nav_publisher_;
 };
 
 int main(int argc, char* argv[]) { return goby::run<BasicPublisher>(argc, argv); }
--- src/intervehicle/basic_subscriber/subscriber.cpp
+++ src/intervehicle/basic_subscriber/subscriber.cpp
@@ -1,3 +1,4 @@
+#include "goby/middleware/protobuf/intervehicle.pb.h"
 #include "goby/zeromq/single-thread-application.h"
 
 #include "messages/groups.h"
@@ -8,20 +9,33 @@
 using Base = goby::zeromq::SingleThreadApplication<BasicSubscriberConfig>;
 using dccl::NavigationReport;
 
+using goby::middleware::intervehicle::protobuf::AckData;
+using goby::middleware::intervehicle::protobuf::ExpireData;
+using goby::middleware::intervehicle::protobuf::Subscription;
+
 class BasicSubscriber : public Base
 {
   public:
     BasicSubscriber()
     {
-        auto nav_callback = [this](const NavigationReport& nav) {
-            std::cout << "Rx: " << nav.DebugString() << std::flush;
+        auto on_nav = [](const NavigationReport& nav) {
+            std::cout << "\nReceived: " << nav.ShortDebugString() << std::endl;
         };
 
-        goby::middleware::protobuf::TransporterConfig nav_subscriber_cfg;
-        nav_subscriber_cfg.mutable_intervehicle()->add_publisher_id(1);
-        nav_subscriber_cfg.mutable_intervehicle()->add_publisher_id(11);
+        auto on_nav_subscribed = [](const Subscription& sub, const AckData& ack) {
+            std::cout << "Received acknowledgment:\n\t" << ack.ShortDebugString()
+                      << "\nfor subscription:\n\t" << sub.ShortDebugString() << std::endl;
+        };
+
+        auto on_nav_subscribe_expired = [](const Subscription& sub, const ExpireData& expire) {
+            std::cout << "Received expiration notice:\n\t" << expire.ShortDebugString()
+                      << "\nfor subscription:\n\t" << sub.ShortDebugString() << std::endl;
+        };
+
+        goby::middleware::Subscriber<NavigationReport> nav_subscriber(
+            cfg().nav_subscriber_cfg(), on_nav_subscribed, on_nav_subscribe_expired);
         
-        intervehicle().subscribe<groups::nav, NavigationReport>(nav_callback, {nav_subscriber_cfg});
+        intervehicle().subscribe<groups::nav, NavigationReport>(on_nav, nav_subscriber);
     }
 };
 
