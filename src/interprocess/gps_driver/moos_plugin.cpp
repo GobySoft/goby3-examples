29c8c1ba18ba2eba8ecbc86025d227168bc91659
Toby Schneider <toby@gobysoft.org>
2019-07-15 13:28:55 -0400

    Updated moos plugin for changes in Goby3 and added launch file

--- src/interprocess/gps_driver/moos_plugin.cpp
+++ src/interprocess/gps_driver/moos_plugin.cpp
@@ -15,15 +15,16 @@ class NavTranslation : public goby::moos::Translator
 
     NavTranslation(const goby::apps::moos::protobuf::GobyMOOSGatewayConfig& cfg) : Base(cfg)
     {
-        this->goby_comms().interprocess().subscribe<groups::gps_data, GPSPosition>(
+        goby().interprocess().subscribe<groups::gps_data, GPSPosition>(
             [this](const GPSPosition& pos) { this->goby_to_moos(pos); });
-
-        this->add_moos_trigger("TEST_LATITUDE");
-        this->add_moos_buffer("TEST_LONGITUDE");
+        
+        moos().add_buffer("TEST_LONGITUDE");
+        moos().add_trigger("TEST_LATITUDE",
+                                 [this](const CMOOSMsg& moos_msg) { moos_to_goby(moos_msg); });
     }
 
   private:
-    void moos_to_goby(const CMOOSMsg& moos_msg) override
+    void moos_to_goby(const CMOOSMsg& moos_msg)
     {
         const auto& key = moos_msg.GetKey();
 
@@ -34,8 +35,8 @@ class NavTranslation : public goby::moos::Translator
                 GPSPosition pos;
                 pos.set_time(moos_msg.GetTime());
                 pos.set_latitude(moos_msg.GetDouble());
-                pos.set_longitude(Base::moos_buffer().at("TEST_LONGITUDE").GetDouble());
-                this->goby_comms().interprocess().publish<groups::gps_data>(pos);
+                pos.set_longitude(moos().buffer().at("TEST_LONGITUDE").GetDouble());
+                goby().interprocess().publish<groups::gps_data>(pos);
                 glog.is(DEBUG1) && glog << "Goby: gps_data: " << pos.ShortDebugString()
                                         << std::endl;
             }
@@ -51,23 +52,25 @@ class NavTranslation : public goby::moos::Translator
     void goby_to_moos(const GPSPosition& pos)
     {
         glog.is(DEBUG1) && glog << "MOOS: GPS_LATITUDE: " << pos.latitude() << std::endl;
-        Base::moos_comms().Notify("GPS_LATITUDE", pos.latitude());
+        moos().comms().Notify("GPS_LATITUDE", pos.latitude());
 
         glog.is(DEBUG1) && glog << "MOOS: GPS_LONGITUDE: " << pos.longitude() << std::endl;
-        Base::moos_comms().Notify("GPS_LONGITUDE", pos.longitude());
+        moos().comms().Notify("GPS_LONGITUDE", pos.longitude());
     }
 };
 
 extern "C"
 {
     void goby3_moos_gateway_load(
-        goby::zeromq::MultiThreadApplication<goby::apps::moos::protobuf::GobyMOOSGatewayConfig>* handler)
+        goby::zeromq::MultiThreadApplication<goby::apps::moos::protobuf::GobyMOOSGatewayConfig>*
+            handler)
     {
         handler->launch_thread<NavTranslation>();
     }
 
     void goby3_moos_gateway_unload(
-        goby::zeromq::MultiThreadApplication<goby::apps::moos::protobuf::GobyMOOSGatewayConfig>* handler)
+        goby::zeromq::MultiThreadApplication<goby::apps::moos::protobuf::GobyMOOSGatewayConfig>*
+            handler)
     {
         handler->join_thread<NavTranslation>();
     }
