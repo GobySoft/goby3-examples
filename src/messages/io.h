2874145a8e017089502e3cfdc2081811323fead8
Toby Schneider <toby@gobysoft.org>
2019-11-12 19:08:59 -1000

    Add examples that were formerly in the goby source repo

---
+++ src/messages/io.h
@@ -0,0 +1,37 @@
+// Copyright 2009-2018 Toby Schneider (http://gobysoft.org/index.wt/people/toby)
+//                     GobySoft, LLC (2013-)
+//                     Massachusetts Institute of Technology (2007-2014)
+//
+//
+// This file is part of the Goby Underwater Autonomy Project Binaries
+// ("The Goby Binaries").
+//
+// The Goby Binaries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The Goby Binaries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Goby.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <iostream>
+
+#include <google/protobuf/message.h>
+
+namespace google
+{
+namespace protobuf
+{
+std::ostream& operator<<(std::ostream& os, const Message& msg)
+{
+    return os << msg.DebugString() << std::endl;
+}
+} // namespace protobuf
+} // namespace google
