dba934c12bad8fa19faaf44f610f82dcb9a2c905
Toby Schneider <tsaubergine@users.noreply.github.com>
2026-04-08 21:21:05 +1000

    Delete cmake/FindProtobufLocal.cmake

--- cmake/FindProtobufLocal.cmake
+++
@@ -1,4 +0,0 @@
-# FindProtobufLocal.cmake - deprecated
-# This file is no longer used. Protobuf is now found via find_package(Protobuf REQUIRED)
-# using CMake's built-in FindProtobuf module, which provides the protobuf_generate() function.
-
